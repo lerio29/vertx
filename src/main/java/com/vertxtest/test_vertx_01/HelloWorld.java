c37929e55d56a0069d528f8795ee400a6668eba6
Vincent Vilerio <ViVilerio@galaxie.bb>
2016-07-11 09:07:44 +0200

    test sur le path

--- src/main/java/com/vertxtest/test_vertx_01/HelloWorld.java
+++ src/main/java/com/vertxtest/test_vertx_01/HelloWorld.java
@@ -1,6 +1,7 @@
 package com.vertxtest.test_vertx_01;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.ext.web.Route;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.TemplateHandler;
 import io.vertx.ext.web.templ.HandlebarsTemplateEngine;
@@ -8,6 +9,7 @@ import io.vertx.ext.web.templ.TemplateEngine;
 
 public class HelloWorld extends AbstractVerticle {
 
+    public static final String APPLICATION_JSON = "application/json";
     final Router router = Router.router(vertx);
 
     @Override
@@ -15,37 +17,45 @@ public class HelloWorld extends AbstractVerticle {
 
         // In order to use a template we first need to create an engine
         final TemplateEngine engine = HandlebarsTemplateEngine.create();
-         TemplateHandler handler = TemplateHandler.create(engine);
+        TemplateHandler handler = TemplateHandler.create(engine);
 
-         router.get("/dynamic/").handler(handler);
+        router.get("/dynamic/*").handler(handler);
 
-         // Route all GET requests for resource ending in .hbs to the template handler
-         router.getWithRegex(".+\\.hbs").handler(handler);
-         
+        // Route all GET requests for resource ending in .hbs to the template
+        // handler
+        router.getWithRegex(".+\\.hbs").handler(handler);
+
         // Entry point to the application, this will render a custom template.
-//        router.get("/test").handler(ctx -> {
-//            // we define a hardcoded title for our application
-//                ctx.put("name", "Page de test");
-//
-////                 and now delegate to the engine to render it.
-//                engine.render(ctx, "../index.hbs", res -> {
-//                    if (res.succeeded()) {
-//                        ctx.response().end(res.result());
-//                    } else {
-//                        ctx.fail(res.cause());
-//                    }
-//                });
-//            });
-//
-//         router.get("/hello/").handler(
-//         req -> req.response().putHeader("content-type", "text/html")
-//         .end("<html><body><h1>Hello World</h1></body></html>"));
-//        
-//         router.get("/byebye/").handler(
-//         req -> req.response().putHeader("content-type", "text/html")
-//         .end("<html><body><h1>Bye Bye World</h1></body></html>"));
+        // router.get("/test").handler(ctx -> {
+        // // we define a hardcoded title for our application
+        // ctx.put("name", "Page de test");
+        //
+        // // and now delegate to the engine to render it.
+        // engine.render(ctx, "../index.hbs", res -> {
+        // if (res.succeeded()) {
+        // ctx.response().end(res.result());
+        // } else {
+        // ctx.fail(res.cause());
+        // }
+        // });
+        // });
+        //
+//        router.get("/hello/").handler(param -> System.out.println(param.request().getParam("toto").trim()));
+        
 
-        vertx.createHttpServer().requestHandler(router::accept).listen(8080, res -> {
+        router.get("/hello/").handler(
+                req -> {
+                    req.response().putHeader("content-type", "text/html")
+                        .end("<html><body><h1>Hello World</h1></body></html>");
+                System.out.println(req.request().getParam("toto").trim());
+                });
+
+        //
+        // router.get("/byebye/").handler(
+        // req -> req.response().putHeader("content-type", "text/html")
+        // .end("<html><body><h1>Bye Bye World</h1></body></html>"));
+
+        vertx.createHttpServer().requestHandler(router::accept).listen(8989, res -> {
             if (res.succeeded()) {
                 System.out.println("success: " + res.toString());
             } else {
